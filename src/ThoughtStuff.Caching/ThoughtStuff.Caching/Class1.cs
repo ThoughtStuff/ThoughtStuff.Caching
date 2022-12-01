e51b486581020c480633362b374534ca5fb2e533
Jacob Foshee <jacobf@gmail.com>
2022-12-01 13:31:19 -0600

    chore: New netstandard2.0 class library ThoughtStuff.Caching

---
+++ src/ThoughtStuff.Caching/ThoughtStuff.Caching/Class1.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) ThoughtStuff, LLC.
+// Licensed under the ThoughtStuff, LLC Split License.
+
+using System;
+
+namespace ThoughtStuff.Caching
+{
+    public class Class1
+    {
+
+    }
+}
