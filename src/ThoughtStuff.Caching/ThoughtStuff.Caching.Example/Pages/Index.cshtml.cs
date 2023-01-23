61cb82d6d04c962da70b5e292afa74396fc68678
Jacob Foshee <jacobf@gmail.com>
2023-01-23 09:38:32 -0600

    test: Add Example web project ASP .NET Core 7 template

---
+++ src/ThoughtStuff.Caching/ThoughtStuff.Caching.Example/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) ThoughtStuff, LLC.
+// Licensed under the ThoughtStuff, LLC Split License.
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ThoughtStuff.Caching.Example.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ src/ThoughtStuff.Caching/ThoughtStuff.Caching.Example/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) ThoughtStuff, LLC.
+// Licensed under the ThoughtStuff, LLC Split License.
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ThoughtStuff.Caching.Example.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
