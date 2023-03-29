5a97c2463263a4c1828bd2848393954a78c5e376
Jacob Foshee <jacobf@gmail.com>
2023-03-29 12:38:37 -0500

    docs(mgmt): Example enumerating cache entries

--- src/ThoughtStuff.Caching/ThoughtStuff.Caching.Example/Pages/CacheManager.cshtml.cs
+++ src/ThoughtStuff.Caching/ThoughtStuff.Caching.Example/Pages/CacheManager.cshtml.cs
@@ -12,9 +12,11 @@ public class CacheManagerModel : PageModel
     }
 
     public int EntryCount { get; private set; }
+    public IAsyncEnumerable<string> Keys { get; private set; } = AsyncEnumerable.Empty<string>();
 
     public async Task OnGetAsync()
     {
         EntryCount = await _cacheManager.GetCacheEntryCount();
+        Keys = _cacheManager.EnumerateKeys();
     }
 }
